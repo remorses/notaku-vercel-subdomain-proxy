01b9259a4558626fdc082e9c3491c983d521ae01
remorses <beats.by.morse@gmail.com>
2021-11-25 18:53:12 +0100

    use rewrites text

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+let TARGET_URL = process.env.TARGET_URL
+
+if (TARGET_URL && !TARGET_URL.startsWith('http')) {
+    TARGET_URL = `https://${TARGET_URL}`
+}
+
+if (TARGET_URL && TARGET_URL.endsWith('/')) {
+    TARGET_URL = TARGET_URL.slice(0, -1)
+}
+
+/**  @type {import('next').NextConfig} */
 module.exports = {
     swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                source: '/',
+                destination: `${TARGET_URL}/:host`,
+            },
+            {
+                source: '/:path*',
+                destination: `${TARGET_URL}/:host/:path*`,
+            },
+        ]
+    },
 }
--- validate.js
+++ validate.js
@@ -1,9 +1,5 @@
-const { SUBDOMAIN, TARGET_URL, FALLBACK_URL } = process.env
+const { TARGET_URL, FALLBACK_URL } = process.env
 
-if (!SUBDOMAIN) {
-    console.error('SUBDOMAIN env var is required')
-    process.exit(1)
-}
 if (!TARGET_URL) {
     console.error('TARGET_URL env var is required')
     process.exit(1)
