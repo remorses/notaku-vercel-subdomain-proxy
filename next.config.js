4d709c1607473ad0276ab5e423c2d312229368ee
remorses <beats.by.morse@gmail.com>
2021-11-25 19:54:27 +0100

    using docs base path

--- next.config.js
+++ next.config.js
@@ -7,12 +7,28 @@ const subPath = TARGET_URL.includes('docs-base-path.notaku.site')
 /**  @type {import('next').NextConfig} */
 module.exports = {
     swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: `/${subPath}`,
+                permanent: false,
+            },
+            // TODO ignore /docs when redirecting /:path
+            // {
+            //     source: '/:path',
+            //     destination: `/docs/:path`,
+            //     permanent: false,
+            // },
+        ]
+    },
     async rewrites() {
         return {
             beforeFiles: [
                 {
+                    basePath: false,
                     source: '/:path*',
-                    destination: `${TARGET_URL}/${subPath}/:path*`,
+                    destination: `${TARGET_URL}/:path*`,
                 },
             ],
         }
